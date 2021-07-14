eab662233ecefe9ebfe48d53b834732fd2578700
Алексей Ерылкин <alexey3v@gmail.com>
2021-07-14 20:21:31 +0300

    Двусвязный список(недоделан)

---
+++ List/List.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+template<typename T>
+struct Node
+{
+	T data;
+	Node<T>* next = NULL;
+	Node<T>* prev = NULL;
+};
+
+
+template<typename T>
+void ViewForward(Node<T>* head)
+{
+	Node<int>* help = head;
+	while (help != NULL)
+	{
+		cout << help->data << "\t";
+		help = help->next;
+	}
+}
+
+template<typename T>
+void AddNodeEnd(Node<T>*& head, Node<T>*& tail, T data)
+{
+	Node<T>* newNode = new Node<T>;
+	newNode->data = data;
+	if (head == NULL)
+	{
+		head = new Node <T>;
+		tail = new Node <T>;
+		return;
+	}
+	
+	newNode->prev = tail;
+	tail->next = newNode;
+	tail = newNode;
+}
+int main()
+{
+	Node<int>* head;
+	Node<int>* tail;
+	for (int i = 0; i < 3; i++)
+		AddNodeEnd(head, tail, i);
+
+	ViewForward(head);
+
+
+	
+
+}
